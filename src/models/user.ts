40f6b87b947e1c62cc62c9842d17d351678c5e22
Gonzalo Geraci <gonza.geraci@gmail.com>
2024-07-30 20:53:58 -0300

    add users and transactions models

---
+++ src/models/user.ts
@@ -0,0 +1,23 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  phone_number: string;
+  photo: string;
+  wallet: string;
+  code: number;
+}
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone_number: { type: String, required: true },
+  photo: { type: String, required: true },
+  wallet: { type: String, required: true },
+  code: { type: Number, required: true }
+});
+
+const User = model<IUser>('User', userSchema, 'users');
+
+export default User;
