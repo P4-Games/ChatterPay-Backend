788f2aabf5ef477eb75202b95470196e85da704b
gonzageraci <gonza.geraci@gmail.com>
2024-07-31 12:59:33 -0300

    delete unused route

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -17,26 +17,7 @@ const transactionRoutes = async (fastify: FastifyInstance) => {
   fastify.get('/transactions/:id', getTransactionById);
   fastify.put('/transactions/:id', updateTransaction);
   fastify.delete('/transactions/:id', deleteTransaction);
-  fastify.post('/make_transaction/', makeTransaction)
-
-  //Test route for sending a transaction
-  fastify.post<{
-    Body: {
-      userId: string;
-      to: string;
-      tokenAddress: string;
-      amount: string;
-    }
-  }>('/send', async (request, reply) => {
-    const { userId, to, tokenAddress, amount, chain_id } = request.body;
-
-    try {
-      const result = await sendUserOperation(userId, to, tokenAddress, amount, chain_id);
-      reply.send(result);
-    } catch (error: any) {
-      reply.status(500).send({ error: 'Failed to send transaction', details: error.message });
-    }
-  });
-};
+  fastify.post('/make_transaction/', makeTransaction);
+}
 
 export default transactionRoutes;
