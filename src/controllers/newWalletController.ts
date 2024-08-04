3ee3efd511e67a78fc358ff4714f30211d3fee2b
Tomas <info@tmdm.com.ar>
2024-08-04 12:48:55 -0300

    Fix: Cambio a español de los mensajes de error y success del endpoint para crear una cuenta

--- src/controllers/newWalletController.ts
+++ src/controllers/newWalletController.ts
@@ -19,13 +19,13 @@ export const createWallet = async (
         const phone_number = channel_user_id; 
 
         if (!phone_number || phone_number.length > 15) {
-            return reply.status(400).send({ message: "Invalid phone number" });
+            return reply.status(400).send({ message: "Número de teléfono no válido" });
         }
 
         // Check if user already exists
         const existingUser = await User.findOne({ phone_number });
         if (existingUser) {
-            return reply.status(400).send({ message: "User already exists" });
+            return reply.status(400).send({ message: "El usuario ya existe" });
         }
 
         // Create new wallet
@@ -45,13 +45,13 @@ export const createWallet = async (
         await newUser.save();
 
         return reply.status(201).send({
-            message: "Wallet created successfully",
+            message: "La wallet fue creada exitosamente!",
             walletHash: predictedWallet.EOAAddress,
             proxyAddress: predictedWallet.proxyAddress
         });
 
     } catch (error) {
-        console.error("Error creating wallet:", error);
-        return reply.status(500).send({ message: "Internal Server Error" });
+        console.error("Error creando una wallet:", error);
+        return reply.status(500).send({ message: "Error interno del servidor" });
     }
 };
