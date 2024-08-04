45c4d52cabbc8f3000a44eb59cb8fe3f03c621a7
Tomas <info@tmdm.com.ar>
2024-08-04 11:04:04 -0300

    Attempt to fix make transaction

--- src/models/userConversation.ts
+++ src/models/userConversation.ts
@@ -1,4 +1,4 @@
-import mongoose, { connection } from "mongoose";
+import mongoose from "mongoose";
 
 export const userConversationSchema = new mongoose.Schema({
     name: String,
@@ -12,19 +12,3 @@ export const userConversationSchema = new mongoose.Schema({
     messaging_channel: String,
     unread_count: Number,
 });
-
-interface IUserConversation extends mongoose.Document {
-    name: string;
-    channel_user_id: string;
-    created_ts: Date;
-    last_message_ts: Date;
-    cost: number;
-    messages: any[];
-    status: string;
-    control: string;
-    messaging_channel: string;
-    unread_count: number;
-}
-
-// Luego, usa esta interfaz al crear el modelo:
-const UserConversation = connection.model<IUserConversation>('user_conversations', userConversationSchema);
