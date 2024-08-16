9114aa4fcfdc8b56a75e9c1045c94aa07549da34
Tomas <info@tmdm.com.ar>
2024-08-16 00:07:50 -0300

    Added NFTs in balance

--- src/routes/nftRoutes.ts
+++ src/routes/nftRoutes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from 'fastify';
-import { getNFT, mintNFT } from '../controllers/nftController';
+import { getAllNFTs, getNFT, mintNFT } from '../controllers/nftController';
 
 const nftRoutes = async (fastify: FastifyInstance) => {
     fastify.post('/nft/', mintNFT);
+    fastify.get('/nfts/', getAllNFTs);
     fastify.get('/nft/:id', getNFT);
     //fastify.post('/nft/transfer', transferNFT);
 };
