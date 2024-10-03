41b69eed41ce478e8967af1092ab4b388ca8c6f8
gonzageraci <gonza.geraci@gmail.com>
2024-10-02 21:42:22 -0300

    [feat] Add constants file w/ GCP env vars

---
+++ src/constants/constants.ts
@@ -0,0 +1,4 @@
+
+export const GCP_BUCKET_NAME = process.env.GCP_BUCKET_NAME || 'your-default-bucket-name';
+export const GCP_KEYFILE_PATH = process.env.GCP_KEYFILE_PATH || 'path-to-your-keyfile.json';
+
