fed7e0ec632fe2335b69fdd3d60fac45b48e6762
gonzageraci <gonza.geraci@gmail.com>
2024-10-04 07:54:36 -0300

    [chore] move enviroments

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -23,3 +23,10 @@ export const PORT = Number(envPort) || 3000;
 export const MONGO_URI = envMongoUri ?? 'mongodb://localhost:27017/chatterpay';
 export const NFT_UPLOAD_IMAGE_ICP = envNftUploadImageIcp === 'true' ?? true;
 export const NFT_UPLOAD_IMAGE_IPFS = envNftUploadImageIpfs === 'true' ?? true;
+export const GCP_IMAGES = {
+  ChatterPayWallet:"https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayWallet.json",
+  ChatterPayWalletFactory: "https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayWalletFactory.json",
+  ChatterPayNFT: "https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayNFT.json",
+  EntryPoint: "https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/EntryPoint.json",
+  ERC20: "https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ERC20.json",
+}
