fba3004914c9c71fce15d5fd90bcc577de6462bf
dappsar <dapps.ar@gmail.com>
2024-10-05 20:00:53 -0300

    [chore] :lipstick: prettier & linter

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -31,11 +31,14 @@ export {
 export const PORT = Number(envPort) || 3000;
 export const MONGO_URI = envMongoUri ?? 'mongodb://localhost:27017/chatterpay';
 export const GCP_ABIs = {
-  ChatterPayWallet:"https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayWallet.json",
-  ChatterPayWalletFactory: "https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayWalletFactory.json",
-  ChatterPayNFT: "https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayNFT.json",
-  EntryPoint: "https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/EntryPoint.json",
-  ERC20: "https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ERC20.json",
-}
-export const NFT_UPLOAD_IMAGE_ICP = (envNftUploadImageIcp === 'true') || true;
-export const NFT_UPLOAD_IMAGE_IPFS =  (envNftUploadImageIpfs === 'true') || true;
+    ChatterPayWallet:
+        'https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayWallet.json',
+    ChatterPayWalletFactory:
+        'https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayWalletFactory.json',
+    ChatterPayNFT:
+        'https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ChatterPayNFT.json',
+    EntryPoint: 'https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/EntryPoint.json',
+    ERC20: 'https://storage.googleapis.com/chatbot-multimedia/chatterpay/ABIs/ERC20.json',
+};
+export const NFT_UPLOAD_IMAGE_ICP = envNftUploadImageIcp === 'true' || true;
+export const NFT_UPLOAD_IMAGE_IPFS = envNftUploadImageIpfs === 'true' || true;
