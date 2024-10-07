6a25a3505f7a6ad00508f7ed9433d5f48f7dee37
gonzageraci <gonza.geraci@gmail.com>
2024-10-07 12:25:55 -0300

    [feat] add controller to withdraw tokens

--- src/controllers/withdrawController.ts
+++ src/controllers/withdrawController.ts
@@ -1,6 +1,9 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { setWallet } from '../services/walletService';
 import { DEFAULT_CHAIN_ID } from '../constants/environment';
+import Token from '../models/token';
+import { generateUserWallet} from "../controllers/swapController"
+import { setupERC20 } from '../services/walletService';
 
 export async function withdrawFunds(
     request: FastifyRequest<{
@@ -16,6 +19,7 @@ export async function withdrawFunds(
     if (!channel_user_id || !dst_address) {
         return reply.status(400).send({ message: 'Invalid parameters' });
     }
+    const { signer, proxyAddress } = await generateUserWallet(channel_user_id);
     if (!dst_address.startsWith('0x')) {
         return reply.status(400).send({ message: 'Invalid destination address' });
     }
@@ -25,7 +29,20 @@ export async function withdrawFunds(
     } catch (error) {
         console.warn("Function 'withdrawFunds' does not exists:", error);
 
+        const tokens = await Token.find({ chain_id });
+        let txs = [];
+        tokens.map(async (token) => {
+            const tokenContract = await setupERC20(token.address, signer);
+            try {
+                const tx = await tokenContract.transfer(dst_address, await tokenContract.balanceOf(proxyAddress));
+                const txHash = await tx.wait()
+                txs.push(txHash);
+            } catch (error) {
+                console.error('Error transferring funds:', error);
+            }
 
+        });
+    } finally {
+        return reply.send({ message: 'Funds are in process of withdraw' });
     }
-    return reply.send({ message: 'Funds are in process of withdraw' });
 }
