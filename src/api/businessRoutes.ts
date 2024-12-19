ee6ee4253fbd3e054d67d92e8b96390f5a056055
Tomas Di Mauro <tomas@tdm.ar>
2024-12-19 11:32:33 -0300

    Revert "[feat] added business related endpoints"
    This reverts commit 169b6ae517821be42923e1e04bfb666403a73da2.

---
+++ src/api/businessRoutes.ts
@@ -0,0 +1,48 @@
+import { FastifyInstance } from 'fastify';
+
+import {
+    createUser,
+    updateUser,
+    deleteUser,
+    getAllUsers,
+    getUserById,
+} from '../controllers/userController';
+
+/**
+ * Configures routes related to users.
+ * @param {FastifyInstance} fastify - Fastify instance
+ * @returns {Promise<void>}
+ */
+const userRoutes = async (fastify: FastifyInstance): Promise<void> => {
+    /**
+     * Route to create a new user
+     * @route POST /users/
+     */
+    fastify.post('/users/', createUser);
+
+    /**
+     * Route to get all users
+     * @route GET /users/
+     */
+    fastify.get('/users/', getAllUsers);
+
+    /**
+     * Route to get a user by their ID
+     * @route GET /users/:id
+     */
+    fastify.get('/users/:id', getUserById);
+
+    /**
+     * Route to update a user
+     * @route PUT /users/:id
+     */
+    fastify.put('/users/:id', updateUser);
+
+    /**
+     * Route to delete a user
+     * @route DELETE /users/:id
+     */
+    fastify.delete('/users/:id', deleteUser);
+};
+
+export default userRoutes;
