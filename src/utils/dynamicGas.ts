0ce5cd7a9237a502744ce377043314fa31068d98
dappsar <dapps.ar@gmail.com>
2024-12-28 23:57:32 -0300

    [fix] :bug: fix error with estimated gas limit (#223)

--- src/utils/dynamicGas.ts
+++ src/utils/dynamicGas.ts
@@ -1,4 +1,4 @@
-import { ethers, Contract, BigNumber } from 'ethers';
+import { Contract, BigNumber } from 'ethers';
 
 /**
  * Get gas limit for a transaction w/ dynamic gas.
@@ -7,31 +7,36 @@ import { ethers, Contract, BigNumber } from 'ethers';
  * @param methodName - Name of the method to call.
  * @param args - Array of arguments for the method.
  * @param gasBufferPercentage - Percentage of gas to add to the estimated gas.
+ * @param defaultGasLimit - default gas limit: 7000000 (the maximum permitted by arb).
  * @returns Gas limit for the transaction.
  * @throws Error if the method does not exist in the contract.
  * @throws Error if the gas estimation fails.
  */
-
 export async function getDynamicGas(
   contract: Contract,
   methodName: string,
   args: unknown[],
-  gasBufferPercentage: number = 10
+  gasBufferPercentage: number = 10,
+  defaultGasLimit: BigNumber = BigNumber.from('7000000')
 ): Promise<BigNumber> {
-  // Verificar que el método existe en el contrato
-  if (typeof contract[methodName] !== 'function') {
-    throw new Error(`The method ${methodName} doesnt exists in contract.`);
+  try {
+    // Check if the method exists in the contract
+    if (typeof contract[methodName] !== 'function') {
+      throw new Error(`The method ${methodName} doesn't exist in contract.`);
+    }
+
+    // Try to estimate the gas required for the transaction
+    const estimatedGas: BigNumber = await contract.estimateGas[methodName](...args);
+
+    // Apply the buffer to the estimated gas
+    const gasLimit: BigNumber = estimatedGas
+      .mul(BigNumber.from(100 + gasBufferPercentage))
+      .div(BigNumber.from(100));
+    console.log(`Estimated gas limit for ${methodName}:`, gasLimit.toString());
+    return gasLimit;
+  } catch (error) {
+    console.warn(`Gas estimation failed for ${methodName}:`, error);
+    // If the estimation fails, use the default gas limit
+    return defaultGasLimit;
   }
-
-  // Estimar el gas necesario para la transacción
-  const estimatedGas: BigNumber = await contract.estimateGas[methodName](...args);
-
-  // Aplicar el buffer al gas estimado
-  const gasLimit: BigNumber = estimatedGas
-    .mul(BigNumber.from(100 + gasBufferPercentage))
-    .div(BigNumber.from(100));
-  console.log('Gas limit:', gasLimit.toString());
-  return gasLimit;
 }
-
-
