30ff874ab25cfcdf8b4a982634f37c078f12e757
dappsar <dapps.ar@gmail.com>
2024-12-31 03:02:05 -0300

    [feat] :label: add log level types (#237)

--- src/types/logger.ts
+++ src/types/logger.ts
@@ -1,4 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type LogMethod = (...args: any[]) => void;
 
-export type LogLevel = 'log' | 'debug' | 'error' | 'info' | 'warn';
+export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal';
+
+export const validLogLevels = Object.values({
+  trace: 'trace',
+  debug: 'debug',
+  info: 'info',
+  warn: 'warn',
+  error: 'error',
+  fatal: 'fatal'
+}) as LogLevel[];
