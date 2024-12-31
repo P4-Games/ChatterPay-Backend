715b8cfedc2aa7098f1f8a2ae9d3162d9c54e56c
dappsar <dapps.ar@gmail.com>
2024-12-31 00:04:07 -0300

    [feat] :label: add logger types (#236)

---
+++ src/types/logger.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type LogMethod = (...args: any[]) => void;
+
+export type LogLevel = 'log' | 'debug' | 'error' | 'info' | 'warn';
