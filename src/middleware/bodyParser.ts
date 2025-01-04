0269f59777d28fa4794d2b30c9f19c3dd39e49bf
dappsar <dapps.ar@gmail.com>
2025-01-03 22:51:46 -0300

    x

--- src/middleware/bodyParser.ts
+++ src/middleware/bodyParser.ts
@@ -1,7 +1,7 @@
 import querystring from 'querystring';
 import { FastifyError, FastifyRequest, FastifyInstance } from 'fastify';
 
-import { Logger } from '../utils/logger';
+// import { Logger } from '../utils/logger';
 
 /**
  * Parses the request body based on its format (JSON, URL-encoded, or key-value pair).
@@ -11,22 +11,32 @@ import { Logger } from '../utils/logger';
  * @throws {Error} If the body format is unrecognized
  */
 function parseBody(body: string): unknown {
+  console.info('xxxxxxxxxxxx-BODY-2', body)
   body = body.trim();
+  console.info('xxxxxxxxxxxx-BODY-3')
 
   if (body.startsWith('{') || body.startsWith('[')) {
+    console.info('xxxxxxxxxxxx-BODY-4')
     try {
+      console.info('xxxxxxxxxxxx-BODY-5')
       return JSON.parse(body);
     } catch (error) {
-      Logger.warn('JSON parse failed, attempting to fix malformed JSON');
+      console.error('xxxxxxxxxxxx-BODY-6', error)
+      // Logger.warn('JSON parse failed, attempting to fix malformed JSON');
       const fixedBody: string = body.replace(/'/g, '"').replace(/(\w+):/g, '"$1":');
+      console.error('xxxxxxxxxxxx-BODY-7', fixedBody)
       return JSON.parse(fixedBody);
     }
   } else if (body.includes('=')) {
-    Logger.log('Parsing URL-encoded or key-value data');
+    console.error('xxxxxxxxxxxx-BODY-8')
+    // Logger.log('Parsing URL-encoded or key-value data');
     if (!body.includes('&')) {
+      console.error('xxxxxxxxxxxx-BODY-9')
       const [key, value]: string[] = body.split('=');
+      console.error('xxxxxxxxxxxx-BODY-10', value)
       return { [key]: value };
     }
+    console.error('xxxxxxxxxxxx-BODY-11')
     return querystring.parse(body);
   }
 
@@ -48,12 +58,16 @@ export async function setupMiddleware(server: FastifyInstance): Promise<void> {
       done: (err: FastifyError | null, body?: unknown) => void
     ) => {
       try {
+        console.info('xxxxxxxxxxxx-BODY-00', body)
         const parsedBody = parseBody(body);
-        Logger.log('Successfully parsed body:', parsedBody);
+        console.info('xxxxxxxxxxxx-BODY-01', parsedBody)
+        // Logger.log('Successfully parsed body:', parsedBody);
         done(null, parsedBody);
-      } catch (err) {
-        Logger.error('Failed to parse body:', body);
-        done(new Error('Invalid body format') as FastifyError, undefined);
+      } catch (error: unknown) {
+        // Logger.error('Failed to parse body:', body);
+        const messageError = error instanceof Error ? error.message : 'Unknown error'
+        console.error('xxxxxxxxxxxx-BODY-01', messageError)
+        done(new Error(`Invalid body format, error: ${messageError}`) as FastifyError, undefined);
       }
     }
   );
