5938bc4cc6f4a5061000d308c788df42c5f0274a
dappsar <dapps.ar@gmail.com>
2025-01-07 19:24:58 -0300

    [refactor] :recycle: delete old utils folder (#276)

--- src/utils/web3_config.ts
+++
@@ -1,7 +0,0 @@
-import { Web3 } from 'web3';
-
-import { INFURA_API_KEY } from '../config/constants';
-
-const web3 = new Web3(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`);
-
-export default web3;
