5e10483ec1e561535ff21b72b504d8c5231d344d
dappsar <dapps.ar@gmail.com>
2025-01-09 22:29:25 -0300

    [docs] :bulb: update comments in helpers (#276)

--- src/helpers/validationHelper.ts
+++ src/helpers/validationHelper.ts
@@ -1,10 +1,13 @@
 import { ethers } from 'ethers';
 
 /**
- * Function that validates if a given input is a phone number
+ * Function that validates if a given input is a valid phone number.
  *
- * @param input
- * @returns
+ * This function cleans the input by removing all non-numeric characters
+ * and then checks if the remaining string has between 8 and 15 digits.
+ *
+ * @param number - The phone number string to be validated.
+ * @returns `true` if the input is a valid phone number, `false` otherwise.
  */
 export function isValidPhoneNumber(number: string): boolean {
   // Keep only numeric characters
@@ -15,13 +18,27 @@ export function isValidPhoneNumber(number: string): boolean {
 }
 
 /**
- * VAlidate if an strin is valid url
- * @param url
- * @returns
+ * Validates if a given string is a valid URL.
+ *
+ * This function uses a regular expression to check if the provided string
+ * matches the typical structure of a valid URL, including the optional
+ * "http" or "https" scheme and domain name.
+ *
+ * @param url - The URL string to be validated.
+ * @returns `true` if the string is a valid URL, `false` otherwise.
  */
 export const isValidUrl = (url: string): boolean => {
   const urlPattern = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?$/;
   return urlPattern.test(url);
 };
 
+/**
+ * Validates if a given string is a valid Ethereum wallet address.
+ *
+ * This function uses the `ethers.utils.isAddress` method to check whether
+ * the provided string is a valid Ethereum address.
+ *
+ * @param address - The Ethereum wallet address string to be validated.
+ * @returns `true` if the string is a valid Ethereum address, `false` otherwise.
+ */
 export const isValidEthereumWallet = (address: string): boolean => ethers.utils.isAddress(address);
