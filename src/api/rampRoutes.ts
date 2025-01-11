2ce7837ea0ced2bc9738a9e4771e2983b78db191
dappsar <dapps.ar@gmail.com>
2025-01-11 11:10:18 -0300

    [feat] :necktie: add additional ramp checks routes (#297)

--- src/api/rampRoutes.ts
+++ src/api/rampRoutes.ts
@@ -1,14 +1,16 @@
 import { FastifyInstance } from 'fastify';
 
 import {
-  createRampUser,
-  getCryptoPairPrices,
-  getUserRampBalance,
-  getUserRampLimits,
-  getUserRampValidationStatus,
-  rampOff,
   rampOn,
-  uploadRampUserDocuments
+  rampOff,
+  createRampUser,
+  getUserRampLimits,
+  getUserRampBalance,
+  getCryptoPairPrices,
+  checkUsersRampStatus,
+  uploadRampUserDocuments,
+  getUserRampDocumentsStatus,
+  getUserRampValidationStatus
 } from '../controllers/rampController';
 
 /**
@@ -19,10 +21,12 @@ import {
 export const rampRoutes = async (fastify: FastifyInstance): Promise<void> => {
   fastify.post('/ramp/user', createRampUser);
   fastify.post('/ramp/user/:userId/compliance/documents', uploadRampUserDocuments);
+  fastify.get('/ramp/user/:userId/compliance/documents/status', getUserRampDocumentsStatus);
   fastify.get('/ramp/user/:userId/compliance/status', getUserRampValidationStatus);
   fastify.get('/ramp/user/:userId/limits', getUserRampLimits);
   fastify.get('/ramp/user/:userId/balance', getUserRampBalance);
-  fastify.get('/ramp/user/:userId/price', getCryptoPairPrices);
+  fastify.get('/ramp/market/price', getCryptoPairPrices);
   fastify.post('/ramp/on', rampOn);
   fastify.post('/ramp/off', rampOff);
+  fastify.post('/ramp/users/compliance/status/check', checkUsersRampStatus);
 };
