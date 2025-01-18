72547b68b94a0ce048d4d5dd68c4c4052a81da24
dappsar <dapps.ar@gmail.com>
2025-01-17 22:38:08 -0300

    [refactor] :label: add bundle url to blockchain schema (#276)

--- src/models/blockchainModel.ts
+++ src/models/blockchainModel.ts
@@ -4,6 +4,7 @@ export interface IBlockchain extends Document {
   name: string;
   chain_id: number;
   rpc: string;
+  bundlerUrl?: string;
   logo: string;
   explorer: string;
   scan_apikey: string;
@@ -23,6 +24,7 @@ const blockchainSchema = new Schema<IBlockchain>({
   name: { type: String, required: true },
   chain_id: { type: Number, required: true },
   rpc: { type: String, required: true },
+  bundlerUrl: { type: String, required: false },
   logo: { type: String, required: true },
   explorer: { type: String, required: true },
   scan_apikey: { type: String, required: true },
