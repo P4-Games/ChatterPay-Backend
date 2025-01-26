85f7596adb13665b8ac33047970b7b1a4958c441
dappsar <dapps.ar@gmail.com>
2025-01-26 20:55:31 -0300

    [feat] :sparkles: add function to concat phone and user name (#364)

--- src/helpers/formatHelper.ts
+++ src/helpers/formatHelper.ts
@@ -6,3 +6,14 @@
 export function getPhoneNumberFormatted(phone: string): string {
   return phone.replace(/\D/g, '');
 }
+
+/**
+ * Concatenates a phone number with a name if the name is provided.
+ *
+ * @param phoneNumber - The phone number to be displayed.
+ * @param name - The name to be included with the phone number (optional).
+ * @returns A string containing the phone number, optionally concatenated with the name in parentheses.
+ */
+export function formatPhoneNumberWithOptionalName(phoneNumber: string, name?: string): string {
+  return name ? `${phoneNumber} (${name})` : phoneNumber;
+}
