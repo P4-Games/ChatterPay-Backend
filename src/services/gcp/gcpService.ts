bcb31bc773a433439ce25d2dd5ce8beec360168b
dappsar <dapps.ar@gmail.com>
2025-02-22 19:55:32 -0300

    [refactor] :recycle: translate comments to english in getGcpFile (#387)

--- src/services/gcp/gcpService.ts
+++ src/services/gcp/gcpService.ts
@@ -3,19 +3,20 @@ import axios from 'axios';
 import { Logger } from '../../helpers/loggerHelper';
 
 /**
- * Obtiene un archivo desde un bucket de GCP.
+ * Retrieves a file from a GCP bucket.
  *
- * @param {string} urlFile - La URL del archivo en el bucket de GCP.
- * @returns {Promise<unknown>} El contenido del archivo obtenido desde GCP.
- * @throws Lanzará un error si no se puede obtener el archivo.
+ * @param {string} urlFile - The URL of the file in the GCP bucket.
+ * @returns {Promise<unknown>} The content of the file retrieved from GCP.
+ * @throws Throws an error if the file cannot be retrieved.
  */
 export const getGcpFile = async (urlFile: string): Promise<unknown> => {
   try {
+    Logger.log('getGcpFile', `Getting file ${urlFile} from GCP bucket`);
     const response = await axios.get(urlFile);
     const { abi } = response.data;
     return abi;
   } catch (error) {
     Logger.error('getGcpFile', urlFile, (error as Error).message);
-    throw new Error('Error obteniendo archivo de GCP');
+    throw new Error(`Error getting file ${urlFile} from GCP bucket`);
   }
 };
