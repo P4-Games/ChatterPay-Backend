5d2221cb4706d3e1387aeeb11774598c442551be
dappsar <dapps.ar@gmail.com>
2025-03-23 17:43:13 -0300

    [fix] :bug: fix error when save swap transaction in bdd

--- src/controllers/swapController.ts
+++ src/controllers/swapController.ts
@@ -253,7 +253,7 @@ export const swap = async (
     // Save transactions OUT
     Logger.log('swap', 'Updating swap transactions in database.');
     const transactionOut: TransactionData = {
-      tx: executeSwapResult.approveTransactionHash || '0x',
+      tx: executeSwapResult.swapTransactionHash || '0x',
       walletFrom: proxyAddress,
       walletTo: networkConfig.contracts.routerAddress!,
       amount: fromTokensSentInUnits,
