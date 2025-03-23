11116cb232b5820d22e27fc5140883c4afefa20b
dappsar <dapps.ar@gmail.com>
2025-03-23 17:25:52 -0300

    [feat] :sparkles: handler user operations counter in mongoUserService (#427)

--- src/services/mongo/mongoUserService.ts
+++ src/services/mongo/mongoUserService.ts
@@ -156,6 +156,38 @@ export const mongoUserService = {
   },
 
   /**
+   * Updates the operation counter for a specific user and operation type.
+   *
+   * @param phoneNumber - User's phone number identifier
+   * @param operationType - Type of operation (transfer, swap, nft, nftCopy)
+   */
+  updateUserOperationCounter: async (
+    phoneNumber: string,
+    operationType: 'transfer' | 'swap' | 'mint_nft' | 'mint_nft_copy'
+  ): Promise<void> => {
+    const currentDate = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+
+    try {
+      Logger.info(
+        'updateUserOperationCounter',
+        `Updating counter for ${operationType} on date ${currentDate} for user ${phoneNumber}`
+      );
+
+      await UserModel.updateOne(
+        { phone_number: phoneNumber },
+        { $inc: { [`operations_counters.${operationType}.${currentDate}`]: 1 } }
+      );
+    } catch (error) {
+      Logger.error(
+        'updateUserOperationCounter',
+        `Error updating counter for ${operationType}`,
+        (error as Error).message
+      );
+      // avoid throw error
+    }
+  },
+
+  /**
    * Gets user language based on the phone number.
    *
    * @param phoneNumber
