06da1e221c92c5ce5cd5d0c599dd528123921abd
dappsar <dapps.ar@gmail.com>
2025-06-16 16:24:06 -0300

    [refactor] :recycle: move computeWallet to prcService (#535)

--- src/services/web3/rpc/rpcService.ts
+++ src/services/web3/rpc/rpcService.ts
@@ -1,6 +1,22 @@
+import * as crypto from 'crypto';
+import { ethers, BigNumber } from 'ethers';
+
+import { gasService } from '../gasService';
 import { Logger } from '../../../helpers/loggerHelper';
+import { SIGNING_KEY } from '../../../config/constants';
 import { rpcQueueAlchemy, rpcQueuePimlico } from './rpcQueue';
-import { RpcProvider, rpcProviders } from '../../../types/commonType';
+import { IBlockchain } from '../../../models/blockchainModel';
+import { getChatterPayWalletFactoryABI } from '../abiService';
+import { generateWalletSeed } from '../../../helpers/SecurityHelper';
+import { getPhoneNumberFormatted } from '../../../helpers/formatHelper';
+import { mongoBlockchainService } from '../../mongo/mongoBlockchainService';
+import {
+  RpcProvider,
+  rpcProviders,
+  ComputedAddress,
+  PhoneNumberToAddress
+} from '../../../types/commonType';
+import { ChatterPayWalletFactory__factory } from '../../../types/ethers-contracts/factories/ChatterPayWalletFactory__factory';
 
 type Serializable =
   | string
@@ -16,6 +32,35 @@ type RpcCallWrapper<T> = {
   name?: string;
   args?: Serializable[];
 };
+
+/**
+ * Generates a deterministic Ethereum address based on a phone number and a chain ID.
+ *
+ * This function derives an Ethereum address by combining the phone number and chain ID
+ * with the environment-defined seed private key. The result includes the hashed private key,
+ * the private key, and the public key (Ethereum address).
+ *
+ * @param {string} phoneNumber - The phone number to generate the address from.
+ * @param {string} chainId - The chain ID to include in the address generation.
+ * @returns {PhoneNumberToAddress} An object containing:
+ *   - `hashedPrivateKey`: A SHA-256 hash of the generated private key.
+ *   - `privateKey`: The deterministic private key.
+ *   - `publicKey`: The Ethereum address corresponding to the private key.
+ *
+ * @throws {Error} If the seed private key is not found in environment variables.
+ */
+function phoneNumberToAddress(phoneNumber: string, chainId: string): PhoneNumberToAddress {
+  const privateKey = generateWalletSeed(getPhoneNumberFormatted(phoneNumber), chainId);
+  const wallet = new ethers.Wallet(privateKey);
+  const publicKey = wallet.address;
+  const hashedPrivateKey = crypto.createHash('sha256').update(privateKey).digest('hex');
+
+  return {
+    hashedPrivateKey,
+    privateKey,
+    publicKey
+  };
+}
 
 /**
  * Executes an asynchronous RPC function using a rate-limited queue specific to the selected provider.
@@ -66,4 +111,101 @@ export async function wrapRpc<T>(
   }
 
   return result;
+}
+
+/**
+ * Computes the Wallet for a given phone number.
+ *
+ * @param {string} phoneNumber - The phone number to compute the proxy address for.
+ * @returns {Promise<ComputedAddress>} A promise that resolves to an object containing the proxy address,
+ * EOA address, and private key.
+ * @throws {Error} If there's an error in the computation process.
+ */
+export async function computeWallet(phoneNumber: string): Promise<ComputedAddress> {
+  try {
+    const networkConfig: IBlockchain = await mongoBlockchainService.getNetworkConfig();
+    const provider = new ethers.providers.JsonRpcProvider(networkConfig.rpc, {
+      name: networkConfig.name,
+      chainId: networkConfig.chainId
+    });
+
+    const backendSigner = new ethers.Wallet(SIGNING_KEY!, provider);
+    const chatterpayWalletFactoryABI = await getChatterPayWalletFactoryABI();
+    const factory = ChatterPayWalletFactory__factory.connect(
+      networkConfig.contracts.factoryAddress,
+      chatterpayWalletFactoryABI,
+      backendSigner
+    );
+
+    const ownerAddress: PhoneNumberToAddress = phoneNumberToAddress(
+      phoneNumber,
+      networkConfig.chainId.toString()
+    );
+
+    const proxyAddress = await factory.computeProxyAddress(ownerAddress.publicKey, {
+      gasLimit: 1000000
+    });
+    Logger.log('computeWallet', `Computed proxy address: ${proxyAddress}`);
+
+    const code = await provider.getCode(proxyAddress);
+    if (code === '0x') {
+      await wrapRpc(
+        {
+          fn: async () => {
+            Logger.log(
+              'computeWallet',
+              `Creating new wallet for EOA: ${ownerAddress.publicKey}, will result in: ${proxyAddress}.`
+            );
+            const gasLimit = await gasService.getDynamicGas(
+              factory,
+              'createProxy',
+              [ownerAddress.publicKey],
+              20,
+              BigNumber.from('700000')
+            );
+
+            let gasPrice: ethers.BigNumber;
+            try {
+              gasPrice = await provider.getGasPrice();
+            } catch (error) {
+              Logger.warn(
+                'computeWallet',
+                'Fallback gas price used due to getGasPrice() failure:',
+                error
+              );
+              gasPrice = ethers.utils.parseUnits('5', 'gwei');
+            }
+
+            const tx = await factory.createProxy(ownerAddress.publicKey, {
+              gasLimit,
+              gasPrice
+            });
+            Logger.log('computeWallet', `tx: ${tx.hash}`);
+            return tx.wait().then(() => true);
+          },
+          name: 'createProxy',
+          args: [ownerAddress.publicKey]
+        },
+        rpcProviders.ALCHEMY
+      );
+    }
+
+    Logger.log(
+      'computeWallet',
+      JSON.stringify({
+        proxyAddress,
+        EOAAddress: ownerAddress.publicKey
+      })
+    );
+
+    return {
+      proxyAddress,
+      EOAAddress: ownerAddress.publicKey,
+      privateKey: ownerAddress.hashedPrivateKey,
+      privateKeyNotHashed: ownerAddress.privateKey
+    };
+  } catch (error) {
+    Logger.error('computeWallet', error);
+    throw error;
+  }
 }
