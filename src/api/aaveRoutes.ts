d7a2a6c9b1e45580ed172f1ee84a15543b3a9540
dappsar <dapps.ar@gmail.com>
2025-09-06 13:25:10 -0300

    [feat] :poop: write bad code that needs to be improved

--- src/api/aaveRoutes.ts
+++ src/api/aaveRoutes.ts
@@ -7,9 +7,9 @@ import {
 } from '../controllers/aaveController';
 
 const aaveRoutes = async (fastify: FastifyInstance): Promise<void> => {
-  fastify.post('/aave/supply', aaveCreateSupply);
-  fastify.get('/aave/supply', aaveGetSupplyInfo);
-  fastify.put('/aave/supply', aaveRemoveSupply);
+  fastify.post('/aave/create_supply', aaveCreateSupply);
+  fastify.get('/aave/get_supply', aaveGetSupplyInfo);
+  fastify.put('/aave/update_supply', aaveRemoveSupply);
 };
 
 export default aaveRoutes;
