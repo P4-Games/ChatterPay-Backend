f645194941fbaddeb3bad7517aa64e0195d36e14
dappsar <dapps.ar@gmail.com>
2026-01-15 23:42:10 -0300

    [feat] :sparkles: Add security PIN and recovery routes (#655)

---
+++ src/api/securityRoutes.ts
@@ -0,0 +1,47 @@
+import type { FastifyInstance } from 'fastify';
+import {
+  getSecurityQuestions,
+  getSecurityStatus,
+  resetSecurityPin,
+  setSecurityPin,
+  setSecurityRecoveryQuestions,
+  verifySecurityPin
+} from '../controllers/securityController';
+
+export default async function securityRoutes(fastify: FastifyInstance) {
+  /**
+   * @route POST /get_security_status/
+   * Get current security status for a user
+   */
+  fastify.post('/get_security_status/', getSecurityStatus);
+
+  /**
+   * @route POST /get_security_questions/
+   * Get list of available security questions
+   */
+  fastify.post('/get_security_questions/', getSecurityQuestions);
+
+  /**
+   * @route POST /set_security_pin/
+   * Set or update user's security PIN
+   */
+  fastify.post('/set_security_pin/', setSecurityPin);
+
+  /**
+   * @route POST /verify_security_pin/
+   * Verify user's security PIN
+   */
+  fastify.post('/verify_security_pin/', verifySecurityPin);
+
+  /**
+   * @route POST /set_security_recovery_questions/
+   * Set recovery questions and answers
+   */
+  fastify.post('/set_security_recovery_questions/', setSecurityRecoveryQuestions);
+
+  /**
+   * @route POST /reset_security_pin/
+   * Reset PIN using recovery questions
+   */
+  fastify.post('/reset_security_pin/', resetSecurityPin);
+}
