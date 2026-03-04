850cf3f1315ef3502fc9f0a0cc20969e25772377
dappsar <dapps.ar@gmail.com>
2026-03-04 20:46:31 -0300

    [fix] :bug: fix template typing usage in notificationService

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -6,7 +6,6 @@ import { isValidPhoneNumber } from '../helpers/validationHelper';
 
 import type { IBlockchain } from '../models/blockchainModel';
 import {
-  type ITemplateSchema,
   NotificationEnum,
   type NotificationTemplatesTypes,
   type NotificationUtilityConfigType
@@ -70,14 +69,16 @@ export async function getNotificationTemplate(
       return cachedTemplate as { title: string; message: string };
     }
 
-    const notificationTemplates: NotificationTemplatesTypes | null =
-      await mongoTemplateService.getTemplate<ITemplateSchema>(templateEnum.NOTIFICATIONS);
+    const notificationTemplates =
+      await mongoTemplateService.getTemplate<NotificationTemplatesTypes>(
+        templateEnum.NOTIFICATIONS
+      );
+
     if (!notificationTemplates) {
       Logger.warn('getNotificationTemplate', 'Notifications Templates not found');
       return defaultNotification;
     }
 
-    // @ts-expect-error 'expected type error'
     const template = notificationTemplates[typeOfNotification];
 
     if (!template) {
